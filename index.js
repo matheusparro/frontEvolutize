58fecf09c50751660f09f5a3c8612286ee971761
Matheus Parro de Sousa <matheus.parro@evolutize.com.br>
2024-05-07 08:09:29 -0300

    test on render

--- index.js
+++ index.js
@@ -9,15 +9,17 @@ function notifyMe() {
         handleServiceWorker();
       }
     });
+  }else{
+    handleServiceWorker();
   }
 }
 
 function handleServiceWorker() {
-alert("IHRA")
+  alert("IHRA")
   navigator.serviceWorker.register("service-worker.js")
     .then(async (serviceWorker) => {
       serviceWorker.update();
-
+      debugger;
       let subscription = await serviceWorker.pushManager.getSubscription();
       if (!subscription) {
         const publicKeyResponse = await fetch('https://backendevolutize.onrender.com/notification/push/public_key');
@@ -26,25 +28,51 @@ alert("IHRA")
           userVisibleOnly: true,
           applicationServerKey: publicKeyData.publicKey,
         });
+        var string = 'https://backendevolutize.onrender.com/notification/push/register'
+        const teste  = await fetch(string, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ subscription })
+        });
       }
-      // var string = 'https://backendevolutize.onrender.com/notification/push/register'
-      // await fetch(string, {
+      console.log("oi")
+      // var stringF = 'https://backendevolutize.onrender.com/notification/push/send'
+      // console.log(stringF)
+      // await fetch('https://backendevolutize.onrender.com/notification/push/send', {
       //   method: 'POST',
       //   headers: {
       //     'Content-Type': 'application/json'
       //   },
       //   body: JSON.stringify({ subscription })
       // });
-      var stringF = 'https://backendevolutize.onrender.com/notification/push/send'
-      console.log(stringF)
-      await fetch('https://backendevolutize.onrender.com/notification/push/send', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ subscription })
-      });
     });
+}
+function verficarSubscricao() {
+
+
+  if (!('Notification' in window)) {
+    alert('This browser does not support desktop notification');
+  } else if (Notification.permission === 'granted') {
+    navigator.serviceWorker.register("service-worker.js")
+    .then(async (serviceWorker) => {
+      serviceWorker.update();
+      debugger;
+      let subscription = await serviceWorker.pushManager.getSubscription();
+      if (subscription) {
+        alert("JA ESTA SUBSCRITO")
+      }else{
+        alert("NAO ESTA SUBSCRITO")
+      }
+    
+    });
+  }else if(Notification.permission === 'default'){
+    alert("SEM NOTIFICAO - DEFAULT")
+  }else if(Notification.permission === 'denied'){
+    alert("NOTIFICAO BLOQUEADA")
+  }
 }
 
 document.getElementById('subscribeButton').addEventListener('click', notifyMe);
+document.getElementById('verifyButton').addEventListener('click', verficarSubscricao);
